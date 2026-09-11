72a4f0a19550158dabac1f17f9d51aefbb0cada2
Chirag Pratap Singh <b240358@skit.ac.in>
2026-09-11 17:29:02 +0000

    Update Jest version for Node compatibility

--- models/listing.js
+++ models/listing.js
@@ -22,6 +22,7 @@ const listingSchema = new Schema({
         filename: String,
         // type : String,
         // set: (v) => v === "" ? "https://images.unsplash.com/photo-1706362723628-60e8f1929ffe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
+        //1
     },
     location : {
         type : String,
